59af29ef7a6e1a1a0e95e7ac7ac74dfb0005222f
Claudio-Morel <claudio.morel@ug.uchile.cl>
2026-09-05 23:41:23 -0400

    feat: exercises 3.15, 3,16, 3.17, and 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const Register = require('./models/register')
 
 
+// -------- Basic middlewares for thhe API ----------
 const app = express()
 
 morgan.token('body', (request) => {
@@ -18,24 +19,19 @@ const logger = morgan(
   ':method :url :status :res[content-length] - :response-time ms :body'
 )
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(logger)
 
-// Function extracted from https://www.w3schools.com/JS/js_random.asp
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
-
+// -------- Valid API Routes ----------
 app.get('/api/persons', (request, response) => {
   Register.find({}).then(result => {
     response.json(result)
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  Register.findOne({ _id: id })
+app.get('/api/persons/:id', (request, response, next) => {
+  Register.findById(request.params.id)
     .then(register => {
       if (register) {
         response.json(register)
@@ -43,18 +39,16 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
-    .catch(error => {
-      response.status(404).end()
-    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  registry = registry.filter(register => register.id != id)
-
-  response.status(204).end()
+  Register.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -77,18 +71,61 @@ app.post('/api/persons', (request, response) => {
   })
 
   newRegister.save().then(result => {
-    console.log('note saved!')
+    console.log('register saved!')
     response.json(result)
   })
 })
 
-app.get('/api/info', (request, response) => {
-  response.send(`
-    <p>Phonebook has info for ${registry.length} people</p>
-    <p>${Date().toString()}</p>
-  `);
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Register.findById(request.params.id)
+    .then(register => {
+      if (!register) {
+        return response.status(404).end()
+      }
+
+      register.name = name
+      register.number = number
+
+      return register.save().then((updatedRegister) => {
+        response.json(updatedRegister)
+      })
+    })
+    .catch(error => next(error))
 })
 
+app.get('/api/info', (request, response) => {
+  Register.countDocuments({}).then(count => {
+    response.send(`
+      <p>Phonebook has info for ${count} people</p>
+      <p>${Date().toString()}</p>
+    `)
+  })
+})
+
+
+// -------- Unknown Endpoint middleware ----------
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+// -------- Error handler middleware ----------
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// handler of requests that result in errors
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
