c6c45f0f991c15c92bdf2e6788eeca1882b938cb
Claudio-Morel <claudio.morel@ug.uchile.cl>
2026-09-12 13:16:05 -0300

    feat: exercise 4.8

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,42 @@
+const assert = require('node:assert')
+const { test, beforeEach, after } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Primer blog',
+    author: 'Claudio Morel',
+    url: 'https://example.com/cachis',
+    likes: 6,
+  },
+  {
+    title: 'Segundo blog',
+    author: 'Diego Morel',
+    url: 'https://example.com/pigot',
+    likes: 7,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('blogs are returned as JSON', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
