438f81ee125459b1ef79af90190a6d0d7dc7deda
Claudio-Morel <claudio.morel@ug.uchile.cl>
2026-09-13 19:08:09 -0300

    feat: exercise 5.10

--- part5/blog-frontend/src/components/BlogList.jsx
+++ part5/blog-frontend/src/components/BlogList.jsx
@@ -1,11 +1,17 @@
 import Blog from './Blog'
 
-const BlogList = ({ blogs, onLike }) => (
-  <div>
-    {blogs.map(blog =>
-      <Blog key={blog.id} blog={blog} onLike={onLike} />
-    )}
-  </div>
-)
+const BlogList = ({ blogs, onLike }) => {
+  const blogsByLikes = [...blogs].sort((firstBlog, secondBlog) =>
+    secondBlog.likes - firstBlog.likes
+  )
+
+  return (
+    <div>
+      {blogsByLikes.map(blog =>
+        <Blog key={blog.id} blog={blog} onLike={onLike} />
+      )}
+    </div>
+  )
+}
 
 export default BlogList
