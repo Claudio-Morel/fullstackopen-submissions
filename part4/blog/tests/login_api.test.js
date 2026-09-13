818af5766ed02d964e7444249bf0c954f0d47f56
Claudio-Morel <claudio.morel@ug.uchile.cl>
2026-09-13 01:49:36 -0300

    feat: exercise 4.18

---
+++ part4/blog/tests/login_api.test.js
@@ -0,0 +1,82 @@
+const assert = require('node:assert')
+const { test, beforeEach, after } = require('node:test')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+const User = require('../models/user')
+const config = require('../utils/config')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await User.deleteMany({})
+
+  const passwordHash = await bcrypt.hash('thispasswordgoesforallmyhomies', 10)
+  const user = new User({
+    username: 'root',
+    name: 'Jesus',
+    passwordHash,
+  })
+
+  await user.save()
+})
+
+test('login succeeds with valid credentials', async () => {
+  const credentials = {
+    username: 'root',
+    password: 'thispasswordgoesforallmyhomies',
+  }
+
+  const response = await api
+    .post('/api/login')
+    .send(credentials)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.username, credentials.username)
+  assert.strictEqual(response.body.name, 'Jesus')
+  assert.strictEqual(typeof response.body.token, 'string')
+
+  const decodedToken = jwt.verify(response.body.token, config.SECRET)
+  assert.strictEqual(decodedToken.username, credentials.username)
+
+  const user = await User.findOne({ username: credentials.username })
+  assert.strictEqual(decodedToken.id, user.id)
+})
+
+test('login fails with an incorrect password', async () => {
+  const credentials = {
+    username: 'root',
+    password: 'wrongpassword',
+  }
+
+  const response = await api
+    .post('/api/login')
+    .send(credentials)
+    .expect(401)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.error, 'invalid username or password')
+})
+
+test('login fails with an unknown username', async () => {
+  const credentials = {
+    username: 'Devil',
+    password: 'thispasswordgoesforallmydemons',
+  }
+
+  const response = await api
+    .post('/api/login')
+    .send(credentials)
+    .expect(401)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.error, 'invalid username or password')
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
