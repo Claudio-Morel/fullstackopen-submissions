9cbb24a46c21f9c33764e6ff5e20f76dfa901fc2
Claudio-Morel <claudio.morel@ug.uchile.cl>
2026-09-14 00:05:51 -0300

    feat: exercise 5.18

--- part5/blog-e2e/tests/blog_app.spec.js
+++ part5/blog-e2e/tests/blog_app.spec.js
@@ -1,7 +1,17 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
+const user = {
+  name: 'Claudio Morel',
+  username: 'cachis',
+  password: 'password'
+}
+
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: user
+    })
     await page.goto('/')
   })
 
@@ -12,5 +22,27 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill(user.username)
+      await page.getByLabel('password').fill(user.password)
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByRole('button', { name: 'logout' })).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill(user.username)
+      await page.getByLabel('password').fill('wrong password')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('invalid username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByRole('button', { name: 'logout' })).not.toBeVisible()
+    })
   })
 })
