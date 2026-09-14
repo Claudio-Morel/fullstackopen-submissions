175ee2fc2538011b43e65f13b0b9ba4b02c9f856
Claudio-Morel <claudio.morel@ug.uchile.cl>
2026-09-14 00:07:50 -0300

    feat: exercise 5.19

--- part5/blog-e2e/tests/blog_app.spec.js
+++ part5/blog-e2e/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 const user = {
   name: 'Claudio Morel',
@@ -26,23 +27,43 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByLabel('username').fill(user.username)
-      await page.getByLabel('password').fill(user.password)
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, user.username, user.password)
 
       await expect(page.getByRole('button', { name: 'logout' })).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByLabel('username').fill(user.username)
-      await page.getByLabel('password').fill('wrong password')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, user.username, 'wrong password')
 
       const errorDiv = page.locator('.error')
       await expect(errorDiv).toContainText('invalid username or password')
       await expect(errorDiv).toHaveCSS('border-style', 'solid')
       await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
       await expect(page.getByRole('button', { name: 'logout' })).not.toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, user.username, user.password)
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      const blog = {
+        title: 'A blog created by Playwright',
+        author: 'Microsoft',
+        url: 'https://playwright.dev/'
+      }
+
+      await page.getByRole('button', { name: 'create new blog' }).click()
+      await page.getByLabel('title').fill(blog.title)
+      await page.getByLabel('author').fill(blog.author)
+      await page.getByLabel('url').fill(blog.url)
+      await page.getByRole('button', { name: 'create' }).click()
+
+      await expect(
+        page.getByText(`${blog.title} ${blog.author}`, { exact: false })
+      ).toBeVisible()
     })
   })
 })
